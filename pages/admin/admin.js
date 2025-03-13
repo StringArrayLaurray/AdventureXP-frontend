38bab5800a8346fbacb4c62c13a01a673adaa700
Celine Lund Månsson <166127204+Celinelundm@users.noreply.github.com>
2025-03-13 10:59:11 +0100

    Fikset stier og tilfæjet nav bars/ headers

--- pages/admin/admin.js
+++ pages/admin/admin.js
@@ -1,15 +1,21 @@
 activitiesButton = document.querySelector("#activities-button");
 bookingsButton = document.querySelector("#bookings-button");
 employeesButton = document.querySelector("#employees-button");
+shopButton = document.querySelector("#shop-button");
 
 activitiesButton.addEventListener("click", () => {
-    window.location.href = "activities/adminActivities.html";
+    window.location.href = "/AdventureXP-frontend/pages/admin/activities.html";
 })
 
 bookingsButton.addEventListener("click", () => {
-    window.location.href = "adminBooking.html";
+    window.location.href = "/AdventureXP-frontend/pages/admin/booking/adminBooking.html";
 })
 
 employeesButton.addEventListener("click", () => {
-    window.location.href = "adminEmployees.html";
+    window.location.href = "/AdventureXP-frontend/pages/admin/employee/adminEmployees.html";
 })
+
+shopButton.addEventListener("click", () => {
+    window.location.href = "/AdventureXP-frontend/pages/admin/shop/adminShop.html";
+})
+
